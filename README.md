802d82bcf40c1f9be3e627f44227a2b91d41781c
VicSchbt <victoire.schubert@gmail.com>
2024-02-22 17:45:41 +0100

    fix: image import on data

--- README.md
+++ README.md
@@ -29,8 +29,8 @@ Users should be able to:
 
 ### Links
 
-- Solution URL: [Add solution URL here](https://your-solution-url.com)
-- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+- Solution URL: [link](https://your-solution-url.com)
+- Live Site URL: [link](https://vicschbt.github.io/PERSO-fylo-dark-theme-landing-page/)
 
 ## My process
 
--- data/data.js
+++ data/data.js
@@ -5,7 +5,7 @@ export const team = [
     title: "Founder & CEO, Huddle",
     testimonial:
       "Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.",
-    pp: "/images/profile-1.jpg",
+    pp: "./images/profile-1.jpg",
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const team = [
     title: "Founder & CEO, Huddle",
     testimonial:
       "Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.",
-    pp: "/images/profile-2.jpg",
+    pp: "./images/profile-2.jpg",
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export const team = [
     title: "Founder & CEO, Huddle",
     testimonial:
       "Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.",
-    pp: "/images/profile-3.jpg",
+    pp: "./images/profile-3.jpg",
   },
 ];
 
@@ -31,7 +31,7 @@ export const features = [
     label: "Access your files, anywhere",
     descr:
       "The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere.",
-    img: "/images/icon-access-anywhere.svg",
+    img: "./images/icon-access-anywhere.svg",
     alt: "",
   },
   {
@@ -39,7 +39,7 @@ export const features = [
     label: "Security you can trust",
     descr:
       "2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files",
-    img: "/images/icon-security.svg",
+    img: "./images/icon-security.svg",
     alt: "",
   },
   {
@@ -47,7 +47,7 @@ export const features = [
     label: "Real-time collaboration",
     descr:
       "Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.",
-    img: "/images/icon-collaboration.svg",
+    img: "./images/icon-collaboration.svg",
     alt: "",
   },
   {
@@ -55,7 +55,7 @@ export const features = [
     label: "Store any type of file",
     descr:
       "Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared.",
-    img: "/images/icon-any-file.svg",
+    img: "./images/icon-any-file.svg",
     alt: "",
   },
 ];
